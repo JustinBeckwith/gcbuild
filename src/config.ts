c52514e60d09a468909db70fa74b9d85d4dc5869
Justin Beckwith <justin.beckwith@discordapp.com>
2023-07-28 11:22:12 -0700

    chore: move from gts to xo (#193)

--- src/config.ts
+++ src/config.ts
@@ -1,75 +1,88 @@
-import fs from 'fs';
-import {cloudbuild_v1} from 'googleapis';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
+import {type cloudbuild_v1} from 'googleapis';
 import yaml from 'js-yaml';
 
-export interface GetConfigOptions {
-  configPath?: string;
-  sourcePath: string;
-  projectId: string;
-  tag?: string;
-}
+export type GetConfigOptions = {
+	configPath?: string;
+	sourcePath: string;
+	projectId: string;
+	tag?: string;
+};
 
-export async function getConfig(opts: GetConfigOptions) {
-  // If no config path was provided, work through the following set of default
-  // paths looking for some kind of config:
-  // 1. cloudbuild.yaml
-  // 2. cloudbuild.json
-  // 3. Dockerfile
-  if (!opts.configPath) {
-    const files = ['cloudbuild.yaml', 'cloudbuild.json', 'Dockerfile'];
-    for (const file of files) {
-      const fullpath = path.join(opts.sourcePath, file);
-      const fileExists = await exists(fullpath);
-      if (fileExists) {
-        opts.configPath = fullpath;
-        break;
-      }
-    }
-  }
+export async function getConfig(options: GetConfigOptions) {
+	// If no config path was provided, work through the following set of default
+	// paths looking for some kind of config:
+	// 1. cloudbuild.yaml
+	// 2. cloudbuild.json
+	// 3. Dockerfile
+	if (!options.configPath) {
+		const files = ['cloudbuild.yaml', 'cloudbuild.json', 'Dockerfile'];
+		for (const file of files) {
+			const fullpath = path.join(options.sourcePath, file);
+			// eslint-disable-next-line no-await-in-loop
+			const fileExists = await exists(fullpath);
+			if (fileExists) {
+				options.configPath = fullpath;
+				break;
+			}
+		}
+	}
 
-  if (!opts.configPath) {
-    throw new Error(`
+	if (!options.configPath) {
+		throw new Error(`
       Unable to find configuration file. Please provide a cloudbuild.yaml,
       cloudbuild.json, or Dockerfile in the source directory.`);
-  }
+	}
 
-  let config: cloudbuild_v1.Schema$Build;
-  if (path.basename(opts.configPath) === 'Dockerfile') {
-    if (!opts.tag) {
-      opts.tag = path.basename(opts.sourcePath);
-    }
-    config = {
-      steps: [
-        {
-          name: 'gcr.io/cloud-builders/docker',
-          args: ['build', '-t', `gcr.io/${opts.projectId}/${opts.tag}`, '.'],
-        },
-      ],
-      images: [`gcr.io/${opts.projectId}/${opts.tag}`],
-    };
-  } else {
-    const configFileContents = await fs.promises.readFile(
-      opts.configPath,
-      'utf8'
-    );
-    const ext = path.extname(opts.configPath);
-    switch (ext) {
-      case '.json':
-        config = JSON.parse(configFileContents);
-        break;
-      case '.yaml':
-        config = (await yaml.load(
-          configFileContents
-        )) as cloudbuild_v1.Schema$Build;
-        break;
-      default:
-        throw new Error(
-          `The ${ext} extension is not supported.  Please pass yaml or json.`
-        );
-    }
-  }
-  return config;
+	let config: cloudbuild_v1.Schema$Build;
+	if (path.basename(options.configPath) === 'Dockerfile') {
+		if (!options.tag) {
+			options.tag = path.basename(options.sourcePath);
+		}
+
+		config = {
+			steps: [
+				{
+					name: 'gcr.io/cloud-builders/docker',
+					args: [
+						'build',
+						'-t',
+						`gcr.io/${options.projectId}/${options.tag}`,
+						'.',
+					],
+				},
+			],
+			images: [`gcr.io/${options.projectId}/${options.tag}`],
+		};
+	} else {
+		const configFileContents = await fs.promises.readFile(
+			options.configPath,
+			'utf8',
+		);
+		const ext = path.extname(options.configPath);
+		switch (ext) {
+			case '.json': {
+				config = JSON.parse(configFileContents) as cloudbuild_v1.Schema$Build;
+				break;
+			}
+
+			case '.yaml': {
+				config = (await yaml.load(
+					configFileContents,
+				)) as cloudbuild_v1.Schema$Build;
+				break;
+			}
+
+			default: {
+				throw new Error(
+					`The ${ext} extension is not supported.  Please pass yaml or json.`,
+				);
+			}
+		}
+	}
+
+	return config;
 }
 
 /**
@@ -77,10 +90,10 @@ export async function getConfig(opts: GetConfigOptions) {
  * @param file path to the file to check
  */
 async function exists(file: string) {
-  try {
-    await fs.promises.access(file, fs.constants.F_OK);
-    return true;
-  } catch (e) {
-    return false;
-  }
+	try {
+		await fs.promises.access(file, fs.constants.F_OK);
+		return true;
+	} catch {
+		return false;
+	}
 }
--- test/test.ts
+++ test/test.ts
@@ -1,219 +1,227 @@
-import assert from 'assert';
+import assert from 'node:assert';
+import fs from 'node:fs';
+import path from 'node:path';
 import {describe, it, afterEach} from 'mocha';
 import chalk from 'chalk';
-import fs from 'fs';
 import nock from 'nock';
-import path from 'path';
-import sinon from 'sinon';
-import {GaxiosOptions, request} from 'gaxios';
-
-import {Builder, BuildError} from '../src/index.js';
+import * as sinon from 'sinon';
+import {type GaxiosOptions, request} from 'gaxios';
+import {Builder, type BuildError} from '../src/index.js';
 import {getConfig} from '../src/config.js';
 
 describe('gcbuild', () => {
-  nock.disableNetConnect();
+	nock.disableNetConnect();
 
-  afterEach(() => {
-    nock.cleanAll();
-    sinon.restore();
-  });
+	afterEach(() => {
+		nock.cleanAll();
+		sinon.restore();
+	});
 
-  describe('🙈 ignore rules', () => {
-    it('should return 0 rules if no .gcloudignore is available', async () => {
-      const builder = new Builder();
-      const rules = await builder.getIgnoreRules();
-      assert.deepStrictEqual(rules, []);
-    });
+	describe('🙈 ignore rules', () => {
+		it('should return 0 rules if no .gcloudignore is available', async () => {
+			const builder = new Builder();
+			const rules = await builder.getIgnoreRules();
+			assert.deepStrictEqual(rules, []);
+		});
 
-    it('should return expected rules if .gcloudignore is available', async () => {
-      const expected = [
-        '.gcloudignore',
-        '.git',
-        '.gitignore',
-        'node_modules',
-        'test/',
-      ];
-      const gcloudignore = path.resolve('test/fixtures/.gcloudignore');
-      await new Promise((resolve, reject) => {
-        fs.createReadStream(gcloudignore)
-          .pipe(fs.createWriteStream('.gcloudignore'))
-          .on('close', resolve)
-          .on('error', reject);
-      });
-      const builder = new Builder();
-      const rules = await builder.getIgnoreRules();
-      fs.unlinkSync('.gcloudignore');
-      assert.deepStrictEqual(rules, expected);
-    });
-  });
+		it('should return expected rules if .gcloudignore is available', async () => {
+			const expected = [
+				'.gcloudignore',
+				'.git',
+				'.gitignore',
+				'node_modules',
+				'test/',
+			];
+			const gcloudignore = path.resolve('test/fixtures/.gcloudignore');
+			await new Promise((resolve, reject) => {
+				fs.createReadStream(gcloudignore)
+					.pipe(fs.createWriteStream('.gcloudignore'))
+					.on('close', resolve)
+					.on('error', reject);
+			});
+			const builder = new Builder();
+			const rules = await builder.getIgnoreRules();
+			fs.unlinkSync('.gcloudignore');
+			assert.deepStrictEqual(rules, expected);
+		});
+	});
 
-  describe('📦 pack & upload', () => {
-    it('should create a GCS bucket if the expected one does not exist', async () => {
-      const scopes = [
-        mockBucketNotExists(),
-        mockBucketCreate(),
-        mockUpload(),
-        mockBuild(),
-        mockPoll(),
-        mockLogFetch(),
-      ];
-      const sourcePath = path.resolve('test/fixtures');
-      const builder = new Builder({sourcePath});
-      sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
-      sinon.stub(builder.auth, 'getClient').resolves({
-        request: async (options: GaxiosOptions) => {
-          return request(options);
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any);
-      const result = await builder.build();
-      scopes.forEach(s => s.done());
-      assert.ok(result.metadata);
-    });
+	describe('📦 pack & upload', () => {
+		it('should create a GCS bucket if the expected one does not exist', async () => {
+			const scopes = [
+				mockBucketNotExists(),
+				mockBucketCreate(),
+				mockUpload(),
+				mockBuild(),
+				mockPoll(),
+				mockLogFetch(),
+			];
+			const sourcePath = path.resolve('test/fixtures');
+			const builder = new Builder({sourcePath});
+			sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			sinon.stub(builder.auth, 'getClient').resolves({
+				async request(options: GaxiosOptions) {
+					return request(options);
+				},
+			} as any);
+			const result = await builder.build();
+			for (const s of scopes) {
+				s.done();
+			}
 
-    it('should PUT the file to Google Cloud Storage', async () => {
-      new Builder();
-    });
-  });
+			assert.ok(result.metadata);
+		});
 
-  describe('🚨 error handing', () => {
-    it('should include a log with an error', async () => {
-      const scopes = [
-        mockBucketExists(),
-        mockUpload(),
-        mockBuild(),
-        mockPollError(),
-        mockLogFetch(),
-      ];
-      const sourcePath = path.resolve('test/fixtures');
-      const builder = new Builder({sourcePath});
-      sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
-      sinon.stub(builder.auth, 'getClient').resolves({
-        request: async (options: GaxiosOptions) => {
-          return request(options);
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any);
-      try {
-        await builder.build();
-        assert.fail('Expected to throw.');
-      } catch (e) {
-        const err = e as BuildError;
-        assert.ok(err.log);
-        assert.ok(
-          err.log!.includes('🌳'),
-          `
+		it('should PUT the file to Google Cloud Storage', async () => {
+			const builder = new Builder();
+		});
+	});
+
+	describe('🚨 error handing', () => {
+		it('should include a log with an error', async () => {
+			const scopes = [
+				mockBucketExists(),
+				mockUpload(),
+				mockBuild(),
+				mockPollError(),
+				mockLogFetch(),
+			];
+			const sourcePath = path.resolve('test/fixtures');
+			const builder = new Builder({sourcePath});
+			sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			sinon.stub(builder.auth, 'getClient').resolves({
+				async request(options: GaxiosOptions) {
+					return request(options);
+				},
+			} as any);
+			try {
+				await builder.build();
+				assert.fail('Expected to throw.');
+			} catch (error) {
+				const error_ = error as BuildError;
+				assert.ok(error_.log);
+				assert.ok(
+					error_.log.includes('🌳'),
+					`
             Expected to match:
               ${chalk.green('🌳')}
-              ${chalk.red(err.log!)}
-          `
-        );
-      }
-      scopes.forEach(s => s.done());
-    });
-  });
+              ${chalk.red(error_.log)}
+          `,
+				);
+			}
 
-  describe('🌳 config', () => {
-    it('should find a Dockerfile if provided', async () => {
-      const config = await getConfig({
-        sourcePath: path.resolve('test/fixtures/docker'),
-        tag: 'taggy',
-        projectId: 'el-gato',
-      });
-      assert.strictEqual(config.steps![0].name, 'gcr.io/cloud-builders/docker');
-    });
+			for (const s of scopes) {
+				s.done();
+			}
+		});
+	});
 
-    it('should throw an error if an unexpected config path is provided', async () => {
-      await assert.rejects(
-        getConfig({
-          sourcePath: path.resolve('test/fixtures/docker'),
-          configPath: path.resolve('test/fixtures/docker/index.js'),
-          projectId: 'el-gato',
-        }),
-        /extension is not supported/
-      );
-    });
-  });
+	describe('🌳 config', () => {
+		it('should find a Dockerfile if provided', async () => {
+			const config = await getConfig({
+				sourcePath: path.resolve('test/fixtures/docker'),
+				tag: 'taggy',
+				projectId: 'el-gato',
+			});
+			assert.strictEqual(config.steps![0].name, 'gcr.io/cloud-builders/docker');
+		});
 
-  describe('🏁 end to end', () => {
-    it('should work together end to end', async () => {
-      const scopes = [
-        mockBucketExists(),
-        mockUpload(),
-        mockBuild(),
-        mockPoll(),
-        mockLogFetch(),
-      ];
-      const sourcePath = path.resolve('test/fixtures');
-      const builder = new Builder({sourcePath});
-      sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
-      sinon.stub(builder.auth, 'getClient').resolves({
-        request: async (options: GaxiosOptions) => {
-          return request(options);
-        },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any);
-      const result = await builder.build();
-      scopes.forEach(s => s.done());
-      assert.ok(result.metadata);
-    });
-  });
+		it('should throw an error if an unexpected config path is provided', async () => {
+			await assert.rejects(
+				getConfig({
+					sourcePath: path.resolve('test/fixtures/docker'),
+					configPath: path.resolve('test/fixtures/docker/index.js'),
+					projectId: 'el-gato',
+				}),
+				/extension is not supported/,
+			);
+		});
+	});
+
+	describe('🏁 end to end', () => {
+		it('should work together end to end', async () => {
+			const scopes = [
+				mockBucketExists(),
+				mockUpload(),
+				mockBuild(),
+				mockPoll(),
+				mockLogFetch(),
+			];
+			const sourcePath = path.resolve('test/fixtures');
+			const builder = new Builder({sourcePath});
+			sinon.stub(builder.auth, 'getProjectId').resolves('el-gato');
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			sinon.stub(builder.auth, 'getClient').resolves({
+				async request(options: GaxiosOptions) {
+					return request(options);
+				},
+			} as any);
+			const result = await builder.build();
+			for (const s of scopes) {
+				s.done();
+			}
+
+			assert.ok(result.metadata);
+		});
+	});
 });
 
 function mockBucketExists() {
-  return nock('https://storage.googleapis.com')
-    .get('/storage/v1/b/el-gato-gcb-staging-bbq')
-    .reply(200);
+	return nock('https://storage.googleapis.com')
+		.get('/storage/v1/b/el-gato-gcb-staging-bbq')
+		.reply(200);
 }
 
 function mockBucketNotExists() {
-  return nock('https://storage.googleapis.com')
-    .get('/storage/v1/b/el-gato-gcb-staging-bbq')
-    .reply(404);
+	return nock('https://storage.googleapis.com')
+		.get('/storage/v1/b/el-gato-gcb-staging-bbq')
+		.reply(404);
 }
 
 function mockBucketCreate() {
-  return nock('https://storage.googleapis.com')
-    .post('/storage/v1/b?project=el-gato', {
-      name: 'el-gato-gcb-staging-bbq',
-      lifecycle: {
-        rule: [{action: {type: 'Delete'}, condition: {age: 1}}],
-      },
-    })
-    .reply(200);
+	return nock('https://storage.googleapis.com')
+		.post('/storage/v1/b?project=el-gato', {
+			name: 'el-gato-gcb-staging-bbq',
+			lifecycle: {
+				rule: [{action: {type: 'Delete'}, condition: {age: 1}}],
+			},
+		})
+		.reply(200);
 }
 
 function mockUpload() {
-  return nock('https://storage.googleapis.com')
-    .post(url => {
-      return url.includes('/storage/v1/b/el-gato-gcb-staging-bbq/o?name=');
-    })
-    .reply(200);
+	return nock('https://storage.googleapis.com')
+		.post((url) => {
+			return url.includes('/storage/v1/b/el-gato-gcb-staging-bbq/o?name=');
+		})
+		.reply(200);
 }
 
 function mockBuild() {
-  return nock('https://cloudbuild.googleapis.com')
-    .post('/v1/projects/el-gato/builds')
-    .reply(200, {
-      name: 'not-a-real-operation',
-      metadata: {build: {logsBucket: 'gs://not-a-bucket', id: 'not-an-id'}},
-    });
+	return nock('https://cloudbuild.googleapis.com')
+		.post('/v1/projects/el-gato/builds')
+		.reply(200, {
+			name: 'not-a-real-operation',
+			metadata: {build: {logsBucket: 'gs://not-a-bucket', id: 'not-an-id'}},
+		});
 }
 
 function mockPoll() {
-  return nock('https://cloudbuild.googleapis.com')
-    .get('/v1/not-a-real-operation')
-    .reply(200, {done: true});
+	return nock('https://cloudbuild.googleapis.com')
+		.get('/v1/not-a-real-operation')
+		.reply(200, {done: true});
 }
 
 function mockLogFetch() {
-  return nock('https://storage.googleapis.com')
-    .get('/storage/v1/b/not-a-bucket/o/log-not-an-id.txt?alt=media')
-    .reply(200, '🌳');
+	return nock('https://storage.googleapis.com')
+		.get('/storage/v1/b/not-a-bucket/o/log-not-an-id.txt?alt=media')
+		.reply(200, '🌳');
 }
 
 function mockPollError() {
-  return nock('https://cloudbuild.googleapis.com')
-    .get('/v1/not-a-real-operation')
-    .reply(200, {error: '💩'});
+	return nock('https://cloudbuild.googleapis.com')
+		.get('/v1/not-a-real-operation')
+		.reply(200, {error: '💩'});
 }
